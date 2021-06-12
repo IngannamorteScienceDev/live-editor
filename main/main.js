888b578b7a82d1299aac6818d1500ad595f418b9
Galyamin <63673861+imyounggod@users.noreply.github.com>
2021-06-12 22:19:43 +0300

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,7 +1,8 @@
 let snippetsjs = document.getElementById('snippetsjs')
-preview = document.getElementById('preview')
+let preview = document.getElementById('preview')
 let buttonGenerateCSS = document.getElementById('generateCSS')
 let filter = document.querySelector('.js-filter')
+let lastEdit;
 let block;
 const microHTML = html => `<!DOCTYPE html>
 <html lang="ru">
@@ -9,43 +10,36 @@ const microHTML = html => `<!DOCTYPE html>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="путь к файлу css" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <title>Stitches</title>
+    <link href="dist/framework.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" 
+    crossorigin="anonymous">
+    <title>micro.css Template</title>
   </head>
   <body>${html}</body>
 </html>`;
 
-
-
 for(let snippet of snippets){
     let figure = document.createElement('figure')
     let img = document.createElement('img')
-    figure.setAttribute('id',snippet.name)
-    figure.setAttribute('draggable',true)
-    img.setAttribute('draggable',false)
     
-    for(let classValue of ['m-0','w-1/2','p-2']){
-        figure.classList.add(classValue)
+    for(let classValue of ['m-0','w-1/2','p-2','snippet-js', snippet.class]){
+      figure.classList.add(classValue)
     }
     
-    //img.classList.add('m-auto')
-    img.classList.add('h-auto')
-    img.classList.add('w-full')
-    img.classList.add('border-1')
-    img.classList.add('border-solid')
-    img.classList.add('border-gray-400')
-    img.classList.add('border-1:hover')
-    img.classList.add('border-solid:hover')
-    img.classList.add('border-black:hover')
+    for(let classValue of ['h-auto','w-full','border-1','border-solid','border-gray-400','border-black:hover']){
+      img.classList.add(classValue)
+    }
     
-    figure.classList.add(`snippet-js`)
-    figure.classList.add(snippet.class)
-    img.src = `/images/${snippet.image}`
+    img.setAttribute('draggable',false)
+    img.src = `./images/${snippet.image}`
+    
+    figure.setAttribute('id',snippet.name)
+    figure.setAttribute('draggable',true)
+
     snippetsjs.appendChild(figure)
     figure.appendChild(img)
 }
-
 
 function masonry(grid, gridCell, gridGutter, dGridCol, tGridCol, mGridCol) {
   var g = document.getElementById(grid),
@@ -58,14 +52,15 @@ function masonry(grid, gridCell, gridGutter, dGridCol, tGridCol, mGridCol) {
     gHeight += gc[i].offsetHeight + parseInt(gridGutter);
   }
 
-  if (window.screen.width >= 1024)
+  if (window.screen.width >= 1200)
     g.style.height = gHeight / dGridCol + gHeight / (gcLength + 1) + 100 + "px";
-  else if (window.screen.width < 1024 && window.screen.width >= 768)
+  else if (window.screen.width < 1200 && window.screen.width >= 768)
     g.style.height = gHeight / tGridCol + gHeight / (gcLength + 1) + "px";
   else g.style.height = gHeight / mGridCol + gHeight / (gcLength + 1) + "px";
 }
 
 let jssnippet = document.querySelectorAll('.snippet-js')
+
 filter.addEventListener("click", event => {
     if (event.target.tagName !== "BUTTON") {
       return;
@@ -75,42 +70,35 @@ filter.addEventListener("click", event => {
     for (var i = 0; i < jssnippet.length; i++) {
       if (jssnippet[i].classList.contains(val)) {
         jssnippet[i].style.display = "block";
-      } else {
-          jssnippet[i].style.display = "none";
+      } 
+      else {
+         jssnippet[i].style.display = "none";
       }
     }
 
     masonry("snippetsjs", ".snippet-js", 0, 2, 2, 1);
   });
 
-
-let snipp_s = document.querySelectorAll('.snippet-js')
-for (let snippet of snipp_s){
+for (let snippet of jssnippet){
     snippet.addEventListener(`dragstart`, (evt) => {
-        evt.target.classList.add(`active`);
         block = evt.target.cloneNode(true)
         document.body.appendChild(block)
         block.hidden = true
     })
     snippet.addEventListener(`dragend`, (evt) => {
-        evt.target.classList.remove(`active`);
         if(block){
             document.body.removeChild(block)
+            if(lastEdit){
+              lastEdit.hidden = false
+            }
             block = null
         }
     });
 }
 
-
-
-
 preview.addEventListener(`dragenter`, (evt)=>{
     evt.stopPropagation()
     evt.preventDefault()
-    
-    active = document.querySelector('.active')
-    current = evt.target
-    
 });
 
 preview.addEventListener(`dragover`, (evt)=>{
@@ -122,54 +110,74 @@ preview.addEventListener(`dragleave`, (evt)=>{
 })
 
 preview.addEventListener('drop',(evt)=>{
-    if(!block){
-        return
-    }
-    evt.stopPropagation()
-    evt.preventDefault()
-    current = evt.target
-
+   
+    let active = document.querySelector('.active')
+    let current = evt.target
+    currentCoord = current.getBoundingClientRect()
+    cursosPos = evt.clientY
     isCorrect = (block !== current && current === preview) || current.parentElement.classList.contains('buildet')
-    console.log(current.parentElement)
-    console.log(isCorrect)
     if(!isCorrect){
         return;
     }
+    if(active && active!== undefined){
+      if(current.parentElement.classList.contains('buildet')){
+        if(cursosPos < currentCoord.y + currentCoord.height / 2 ){
+          current.parentElement.before(active)
+          return
+        }
+        else{
+          current.parentElement.after(active)
+          return
+        }
+      }
+    } 
     
-    block.classList.remove('active')
-    block.classList.remove('w-1/2')
-    block.classList.remove('p-2')
-    block.classList.add('p-0')
-    block.classList.add('buildet')
     block.innerHTML+=`<div class='js-delete-btn bg-white hidden absolute top-0 left-1 border-1 border-solid border-gray-400 px-4 py-2 shadow shadow-lg'>
     <i class="far fa-trash-alt"></iс>
     </div>`;
-    block.classList.add('relative')
-    block.childNodes[0].classList.remove('border-1:hover')
-    block.childNodes[0].classList.remove('border-solid:hover')
-    block.childNodes[0].classList.remove('border-black:hover')
-    block.hidden = false
-    if(current.classList.contains('buildet')){
-        preview.insertBefore(block.cloneNode(true),evt.target.parentElement)
+
+    for(let classValue of ['border-1:hover','border-solid:hover','border-black:hover']){
+      block.childNodes[0].classList.remove(classValue)
+    }
+
+    for(let classValue of ['active','w-1/2','p-2']){
+      block.classList.remove(classValue)
+    }
+    
+    for(let classValue of ['relative','p-0','buildet']){
+      block.classList.add(classValue)
+    }
+    
+    if(current.parentElement.classList.contains('buildet')){
+      if(cursosPos < currentCoord.y + currentCoord.height / 2 ){
+        lastEdit = block.cloneNode(true)
+        current.parentElement.before(lastEdit)
         return
       }
+      else{
+        lastEdit = block.cloneNode(true)
+        current.parentElement.after(lastEdit)
+        return
+      }
+    }
+
     preview.appendChild(block.cloneNode(true))
-   
+    preview.lastChild.hidden = false
 })
 
 buttonGenerateCSS.addEventListener('click',(evt)=>{
-    let arr = [];
+    let snippetToHTML = [];
     let htmlToFile = '';
     for (let elem of preview.childNodes){
-        let non = snippets.find(snippet => snippet.name === elem.id)
-        if(non !== undefined){
-            arr.push(non)
+        let snipppetObject = snippets.find(snippet => snippet.name === elem.id)
+        if(snipppetObject !== undefined){
+            snippetToHTML.push(snipppetObject)
         }
     }
-    console.log(arr)
-    for(let snippet of arr){
-        htmlToFile += snippet.content + '\n'
+    for(let snipppetObject of snippetToHTML){
+        htmlToFile += snipppetObject.content + '\n'
     }
+    
     let blob = new Blob([microHTML(htmlToFile)],{type: 'text/html'})
 
     let link = document.createElement('a');
@@ -178,7 +186,6 @@ buttonGenerateCSS.addEventListener('click',(evt)=>{
     link.click();
     URL.revokeObjectURL(link.href);
 })
-
 
 document.addEventListener("click", function (evt) {
   if (evt.target.classList.contains('js-delete-btn')) {
@@ -189,10 +196,17 @@ document.addEventListener("click", function (evt) {
   }
 });
 
+document.addEventListener("dragstart", function (evt) {
+  if (evt.target.classList.contains('buildet')) {
+    evt.target.classList.add('active')
+  }
+});
+document.addEventListener("dragend", function (evt) {
+  if (evt.target.classList.contains('buildet')) {
+    evt.target.classList.remove('active')
+  }
+});
+
 window.addEventListener('load', (evt)=>{
       masonry("snippetsjs", ".snippet-js", 0, 2, 2, 1);
 })
-window.addEventListener('resize', (evt)=>{
-      masonry("snippetsjs", ".snippet-js", 0, 2, 2, 1);
-})
-
